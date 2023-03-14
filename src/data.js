6988e6af6ed71b6ca5267792fb10987d0c52343c
otislei <otislei2020@gmail.com>
2023-03-14 13:40:44 +0800

    feat: restore data

--- src/data.js
+++ src/data.js
@@ -11,6 +11,90 @@ export const links = [
   },
 ];
 
+export const products = [
+  {
+    id: 1,
+    image:
+      "https://images.unsplash.com/photo-1597589827317-4c6d6e0a90bd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80",
+    name: "diamond pyramid",
+    price: 13580,
+    category: "category-1",
+  },
+  {
+    id: 2,
+    image:
+      "https://images.unsplash.com/photo-1525351326368-efbb5cb6814d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80",
+    name: "toasted wheat bread with fried egg",
+    price: 9580,
+    category: "category-2",
+  },
+  {
+    id: 3,
+    image:
+      "https://images.unsplash.com/photo-1562376552-0d160a2f238d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1025&q=80",
+    name: "waffles on plate",
+    price: 1580,
+    category: "category-3",
+  },
+  {
+    id: 4,
+    image:
+      "https://images.unsplash.com/photo-1562376552-0d160a2f238d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1025&q=80",
+    name: "waffles on plate",
+    price: 780,
+    category: "category-3",
+  },
+  {
+    id: 5,
+    image:
+      "https://images.unsplash.com/photo-1525351326368-efbb5cb6814d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80",
+    name: "toasted wheat bread with fried egg",
+    price: 6580,
+    category: "category-2",
+  },
+  {
+    id: 6,
+    image:
+      "https://images.unsplash.com/photo-1597589827317-4c6d6e0a90bd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80",
+    name: "diamond pyramid",
+    price: 11580,
+    category: "category-1",
+  },
+];
+
+export const aboutPics = [
+  {
+    id: 1,
+    image:
+      "https://images.unsplash.com/photo-1677486612550-c3bc528f0e5a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=677&q=80",
+    name: "a woman standing in the middle of a desert",
+  },
+  {
+    id: 2,
+    image:
+      "https://plus.unsplash.com/premium_photo-1664279047130-2cd80d215d3d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+    name: "a man carry a baby",
+  },
+  {
+    id: 3,
+    image:
+      "https://images.unsplash.com/photo-1678213786731-0d78bf749dff?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=688&q=80",
+    name: "a woman is walking through the snow at night",
+  },
+  {
+    id: 4,
+    image:
+      "https://images.unsplash.com/photo-1678273505809-98326af28d0a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+    name: "a man and a woman walking down a sidewalk next to the ocean",
+  },
+  {
+    id: 5,
+    image:
+      "https://plus.unsplash.com/premium_photo-1668197656910-921dc95ecf33?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+    name: "a woman and a man looking at a book",
+  },
+];
+
 export const homeSlidedPics = [
   {
     id: 1,
